9fb67006dc668165f0f20f882c76f1ac235a7ee9
cuidaguoguo <19046725972@qq.com>
2017-12-17 23:15:54 +0800

    116任务卡

--- 116任务/lib/main.js
+++ 116任务/lib/main.js
@@ -3,13 +3,13 @@ function main() {
                  '2. 生成成绩单',
                  '3. 退出');
 
-var answer = require('readline-sync').question("请输入你的选择（1～3）：\n");
+var answer = require('cli-interact').question("请输入你的选择（1～3）：\n");
     switch(answer){
             case '1':
                 addStudentInfo();
                 break;
             case '2':
-                printInfor();
+                printInfo();
                 break;
             case '3':
                 process.exit(1);
@@ -17,10 +17,10 @@ var answer = require('readline-sync').question("请输入你的选择（1～3）
 }
  //添加学生的函数
 function addStudentInfo(){
-	var Info = require('readline-sync').question("请输入学生信息（格式：姓名, 学号, 民族, 班级, 学科: 成绩, ...），按回车提交：\n");
-
+	var Info = require('cli-interact').question("请输入学生信息（格式：姓名, 学号, 民族, 班级, 学科: 成绩, ...），按回车提交：\n");
+	var Name = Info.split(",");
 	if(judgeformat(Info)){
-		console.log("\n\n学生"+ Info.split(",")[0] + "的成绩被添加\n\n");
+		console.log("\n\n学生"+ Name[0] + "的成绩被添加\n\n");
 	}
 	main();
 }
@@ -53,6 +53,23 @@ function judgeformat(Info) {
 
 		studens.push(oneStudent);
 		return true;
+	}
 }
+//打印学生成绩单
+function printInfo(){
+	var transcript = require('cli-interact').question("请输入学生学号（格式： 学号, 学号,...），按回车提交：\n");
+	if(judgetranscript(transcript)){
+		console.log("\n\n学生"+ Name[0] + "的成绩被添加\n\n");
+	}
+	main();
+}
+//检查是否输入的是学号
+function judgetranscript(Info) {
+	var transcript = Info.split(",");
+    for (var i = 0; i < transcript.length; i++) {
+        if (isNaN(parseInt(transcript[i]))) {
+            var Infos = require('cli-interact').question("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+        }
+    }
 }
 module.exports = main;
