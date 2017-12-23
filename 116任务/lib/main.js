081c9b29b6f7e4ae3d753f4bcb6885c26343572b
cuidaguoguo <19046725972@qq.com>
2017-12-23 12:06:41 +0800

    116任务卡

--- 116任务/lib/main.js
+++ 116任务/lib/main.js
@@ -65,11 +65,45 @@ function printInfo(){
 }
 //检查是否输入的是学号
 function judgetranscript(Info) {
-	var transcript = Info.split(",");
+    var transcript = Info.split(",");
     for (var i = 0; i < transcript.length; i++) {
         if (isNaN(parseInt(transcript[i]))) {
             var Infos = require('cli-interact').question("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+            judgetranscript(Infos)
+
         }
     }
+    var scoreList = "\n\n成绩单\n姓名|数学|语文|英语|编程|平均分|总分\n========================\n";
+    for (var i = 0; i < transcript.length; i++) {
+        for (var j = 0; j < studens.length; j++) {
+            if (studens[j].studentId == arr[i]) {
+                scoreList += studens[j].name + "|" + studens[j].math + "|" + studens[j].chinese + "|" + studens[j].English + "|" + studens[j].programme + "|" + studens[j].average + "|" + studens[j].total + "\n";
+                continue;
+            }
+        }
+    }
+    scoreList += getallAverages(studens);
+    console.log(scoreList);
+}
+function getallAverages(Infos){
+	console.log(Infos);
+	var allAverage = 0;
+	var list = [];
+	for(var k = 0;k<Infos.length;k++){
+		allAverage += parseInt(Infos[k].total);
+		list.push(Infos[k].total);
+	}
+	allAverage = allAverage / Infos.length;
+
+	list.sort();
+	var midder = 0;
+	if(list.length % 2 == 0){
+		midder = (list[list.length/2-1] + list[list.length/2]) / 2;
+	}else{
+		var index = Math.ceil(list.length/2)
+		midder = list[index-1];
+	}
+
+return "全班总分平均数："+ allAverage + "\n" + "全班总分中位数：" + median + "\n\n";
 }
 module.exports = main;
